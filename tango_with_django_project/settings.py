804ea31236532e02db66b85182e6bc633d03defb
Victor E <victorevector@gmail.com>
2015-01-08 17:01:30 -0600

    Initialize Template

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+
+STATIC_PATH = os.path.join(BASE_DIR,'static')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -53,6 +57,15 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
+# Templates
+
+TEMPLATE_DIRS = [
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    TEMPLATE_PATH,
+    ]
+
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
@@ -82,3 +95,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    STATIC_PATH,
+    )
+
+# Media files
+
+MEDIA_URL = '/media/' #user uploaded files will be availbe from http://.../media/
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') #tells Django where uploaded files should be stored on local disk
+
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,3 +10,13 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')), #sends 'rango/' string to rango/urls.py, which then sends request to rango/view.py, which dispatches index()
 )
+
+#if settings.py has DEBUG set to TRUE, append additional URL matching pattern to 'urlpatterns'
+# urlpattern states that for any file requested with a URL starting with 'media/', the request
+# will be passed to the django.views.static view. This view handles the dispatching of uploaded media files.
+if settings.DEBUG: 
+	urlpatterns += patterns( 
+		'django.views.static',
+		(r'^media/(?P<path>.*)',
+		'serve',
+		{'document_root': settings.MEDIA_ROOT}), )
