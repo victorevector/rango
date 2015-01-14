0f7003285fef849431a471f805dacac6f2d07d6f
Victor E <victorevector@gmail.com>
2015-01-14 13:55:58 -0800

    Connect Category.model to View & Connect data to template

--- rango/admin.py
+++ rango/admin.py
@@ -2,6 +2,23 @@ from django.contrib import admin
 from rango.models import Category, Page
 
 admin.site.register(Category)
-admin.site.register(Page)
 
+class PageAdmin(admin.ModelAdmin):
+	# sets up the following fields as columns in rango/pages
+	list_display = ['title', 'category','url']
+	# Allows you to display fields within Fieldsets when you select a specific page
+	# And allows you to change vertical ordering of the following fields
+	# fieldsets = [
+	# 	(None,		 {'fields': ['title']}),
+	# 	('Category', {'fields': ['category']}),
+	# 	('URL',		 {'fields': ['url']}),
+	# 	('Views',	 {'fields': ['views']}),
+	# ]
+	fieldsets = [
+		(None, {'fields': ['title',
+						   'category',
+						   'url',
+						   'views']}),
+	]
 
+admin.site.register(Page, PageAdmin)
--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from rango.models import Category
 
 def index(request):
-	# Construct a dictionary to pass to the template engine as its context.
-	# Note the key boldmessage is the same as {{ boldmessage }} in the template!
-	context_dict = {'boldmessage': "I am bold font from the context"}
+	#Query the database for a list of ALL categories currently stored
+	#Order the categories by no. likes in descending order
+	#Retrieve the top 5 only- or all if less than 5
+	#Place the list in our context_dict dictionary which will be passed to the template engine
+	category_list = Category.objects.order_by('-likes')[:5] #'-' responsible for descending order
+	context_dict = {'categories': category_list}
 
 	# Return a rendered response to send to the client
 	# We make use of the shortcut function to make our lives easier.
