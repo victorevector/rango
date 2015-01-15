7819b56ac44add90a7db6b9cc7a4e2000649aad7
lbrindze <lorenbrindze@gmail.com>
2015-01-14 16:08:09 -0800

    fixed migration issues

--- rango/models.py
+++ rango/models.py
@@ -1,19 +1,25 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 class Category(models.Model):
-	name = models.CharField(max_length = 128, unique = True)
-	views = models.IntegerField(default = 0)
-	likes = models.IntegerField(default = 0)
+    name = models.CharField(max_length = 128, unique = True)
+    views = models.IntegerField(default = 0)
+    likes = models.IntegerField(default = 0)
+    slug = models.SlugField(unique=True)
 
-	def __unicode__(self):
-		return self.name
+    def __unicode__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug=slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
 class Page(models.Model):
-	category = models.ForeignKey(Category)
-	title = models.CharField(max_length = 128)
-	url = models.URLField()
-	views = models.IntegerField(default = 0)
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length = 128)
+    url = models.URLField()
+    views = models.IntegerField(default = 0)
 
-	def __unicode__(self):
-		return self.title
-		
+    def __unicode__(self):
+        return self.title
+        
