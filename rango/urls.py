498f347053ff3da3487fdeaba7c877dfc0859293
Victor E <victorevector@gmail.com>
2015-01-29 17:26:04 -0800

    Mapped views.register to url 9.4.4

--- rango/urls.py
+++ rango/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     url(r'^add_category/$', views.add_category, name='add_category'),
     url(r'^category/(<category_name_url>)/add_page/$', views.add_page, name='add_page'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
+    url(r'^register/$', views.register, name='register'),
     )
 #url() invokes views.category if regular expression matches
