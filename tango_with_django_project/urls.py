5d608009c682c237b67dbdeb32d54ba6baee816f
Victor E <victorevector@gmail.com>
2015-02-09 19:18:47 -0800

    modified the registration flow by intercepting registration request and pushing them into a a RegistrationView subclass.

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from registration.backends.simple.views import RegistrationView
+
+#createa a new class that redirects the user to the index page, if succesful at logging
+class MyRegistrationView(RegistrationView):
+    def get_success_url(selfself, request, user):
+        return '/rango/'
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,6 +15,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')), #sends 'rango/' string to rango/urls.py, which then sends request to rango/view.py, which dispatches index()
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
     (r'^accounts/', include('registration.backends.simple.urls')),
 )
 
